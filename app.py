f47fd8eaf3c73bc9874d49e61698f6cb6e73da65
AMER-ADLR <152026156+AMER-ADLR@users.noreply.github.com>
2023-11-29 08:19:07 +0000

    1st-change

--- app.py
+++ app.py
@@ -1,47 +1,33 @@
-from flask import Flask , render_template , jsonify
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-JOBS = [
-          {
-            'id': 1 ,
-            'title':"Data Analysis",
-            'location':"Lahore",
-            'salary':"10,0000"
-               
-          },
-          {  
-            'id': 2 ,
-            'title':"Graphic Desinger",
-            'location':"Lahore",
-            'salary':"15,0000"
-    
-     
-          },
-            
-          {
-            'id':3 ,
-            'title':"Programmer",
-            'location':"remote",
-            
-
-          },
-
-          {
-            'id': 4 ,
-            'title':"DB Administrator",
-            'location':"Lahore",
-            'salary':"20,0000"
-
-          }
-  
-      ]
-
+JOBS = [{
+    'id': 1,
+    'title': "Data Analysis",
+    'location': "Lahore",
+    'salary': "10,0000"
+}, {
+    'id': 2,
+    'title': "Graphic Desinger",
+    'location': "Lahore",
+    'salary': "15,0000"
+}, {
+    'id': 3,
+    'title': "Programmer",
+    'location': "remote",
+}, {
+    'id': 4,
+    'title': "DB Administrator",
+    'location': "Lahore",
+    'salary': "20,0000"
+}]
 
 
 @app.route('/')
 def hello_world():
-  return render_template('home.html', jobs =JOBS, compnay_name ='Iubian')
+  return render_template('home.html', jobs=JOBS, compnay_name='Iubian')
+
 
 @app.route("/api/jobs")
 def list_jobs():
@@ -49,4 +35,4 @@ def list_jobs():
 
 
 if __name__ == '__main__':
-  app.run(host= '0.0.0.0' , debug= True)
+  app.run(host='0.0.0.0', debug=True)
